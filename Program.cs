5241f62acfbeebb9231bc3c29fe36caeae58d81c
Amanojha-newvision <aman.ojha@newvision-software.com>
2023-04-04 18:38:12 +0530

    Added the cors for header

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +23,9 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.UseHttpsRedirection(); 
 
 app.UseAuthorization();
 
